b4b837f0405b7fe940c690b19b2d40d20188b4b3
pinomartin <martin91pino@gmail.com>
2020-07-10 14:22:05 -0300

    Up

--- js/app.js
+++ js/app.js
@@ -5,7 +5,13 @@ const btnDisLike = document.getElementById('btn-dislike');
 const fetchMovie =  async () => {
     const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKEY}&language=es-ES&sort_by=popularity.desc`);
     const data = await response.json();
-    console.log(data)
+    const {results} = data;
+    console.log(results)
+    const {poster_path} = results.find(el => el.id === 157336);
+    const imgUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`
+    console.log(imgUrl)
+
 }
 
 fetchMovie();
+
