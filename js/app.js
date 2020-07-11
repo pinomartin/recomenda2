d8b7ab707ea070adfc9b05ef095282dd45b23f73
EmiCorrea <110947@tecnicatura.frc.utn.edu.ar>
2020-07-11 17:52:37 -0300

    Agrego cambios para devolver pelicula random

--- js/app.js
+++ js/app.js
@@ -6,12 +6,13 @@ const fetchMovie =  async () => {
     const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKEY}&language=es-ES&sort_by=popularity.desc`);
     const data = await response.json();
     const {results} = data;
-    console.log(results)
-    const {poster_path} = results.find(el => el.id === 157336);
-    const imgUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`
-    console.log(imgUrl)
 
+    //Obtiene una película random del array de resultados
+    randomMovie = results[Math.floor(Math.random() * results.length)];
+    //Extrae la imagen del poster
+    poster_path = randomMovie.poster_path;
+    const imgUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+    //Asigna esa imagen a los elementos para visualizar
+    document.getElementById("movieImg").src=imgUrl;
+    document.getElementById("modalHeader").style.backgroundImage = 'url(' + imgUrl + ')';
 }
-
-fetchMovie();
-
